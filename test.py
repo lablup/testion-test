31bbcd1a2067f8bb8cdcc71ae8abf566f25c41af
Joongi Kim <joongi@lablup.com>
2016-10-25 17:32:32 +0900

    Add switches to enable specific tests only.

--- test.py
+++ test.py
@@ -1,20 +1,24 @@
 import unittest
+import os
 
 
-class SuccessCase(unittest.TestCase):
+if os.environ.get('TESTION_SUCCESS', '1') == '1':
+    class SuccessCase(unittest.TestCase):
 
-    def test_good(self):
-        self.assertTrue(1)
+        def test_good(self):
+            self.assertTrue(1)
 
-    def test_best(self):
-        self.assertTrue(2)
+        def test_best(self):
+            self.assertTrue(2)
 
-class FailureCase(unittest.TestCase):
+if os.environ.get('TESTION_FAILURE', '1') == '1':
+    class FailureCase(unittest.TestCase):
 
-    def test_bad(self):
-        self.assertFalse(1)
+        def test_bad(self):
+            self.assertFalse(1)
 
-class ErrorCase(unittest.TestCase):
+if os.environ.get('TESTION_ERROR', '1') == '1':
+    class ErrorCase(unittest.TestCase):
 
-    def test_ough(self):
-        raise ValueError('something wrong')
+        def test_ough(self):
+            raise ValueError('something wrong')
