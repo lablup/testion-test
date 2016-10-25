8bf963e376f9daa1629e83377df9c0e9ad9a0126
Joongi Kim <joongi@lablup.com>
2016-10-25 14:29:19 +0900

    Some dummy test cases.

---
+++ test.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class SuccessCase(unittest.TestCase):
+
+    def test_good(self):
+        self.assertTrue(1)
+
+    def test_best(self):
+        self.assertTrue(2)
+
+class FailureCase(unittest.TestCase):
+
+    def test_bad(self):
+        self.assertFalse(1)
